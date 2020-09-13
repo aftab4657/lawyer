c956dc72d1a0bad80c6636d5a15178686b7f12e6
aftab4657 <aftab4657@gmail.com>
2020-09-13 10:58:43 +0500

    Updates

---
+++ precache-manifest.6ef6bda5cde12d578d540deaa050dab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b497952e5853a433ea18891ccef35f3d",
+    "url": "/lawyer/index.html"
+  },
+  {
+    "revision": "1618a1f22facf4ff159d",
+    "url": "/lawyer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90c742a84f74fe0b5c0b",
+    "url": "/lawyer/static/js/2.155cf698.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lawyer/static/js/2.155cf698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1618a1f22facf4ff159d",
+    "url": "/lawyer/static/js/main.143040a0.chunk.js"
+  },
+  {
+    "revision": "6a1d45463cc27710e729",
+    "url": "/lawyer/static/js/runtime-main.dcd8f4f6.js"
+  }
+]);
